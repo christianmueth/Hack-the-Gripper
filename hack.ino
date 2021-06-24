fdddbf9dded2128680bae9986e0e2256e756d4e6
Christian Mueth <christianelijahmueth@gmail.com>
2021-06-24 19:20:39 -0400

    Add files via upload

--- hack.ino
+++ hack.ino
@@ -1,21 +1,30 @@
+//For arduino uno
+
 #include <Stepper.h>
+
 #include<Servo.h>
-const int stepsPerRevolution = 200;
-Stepper myStepper(stepsPerRevolution, 10, 11, 12,13);
-Stepper myStepper2(stepsPerRevolution, 6, 7, 8, 9);
-Stepper myStepper3(stepsPerRevolution, 2, 3, 4, 5);
-Servo ser1;
+
+const int stepsPerRevolution = 200;//Define # of steps in stepper rotation
+
+Stepper myStepper1(stepsPerRevolution, 10, 11, 12,13);//define stepper 1 at given pins (10, 11, 12, 13)
+
+Stepper myStepper2(stepsPerRevolution, 6, 7, 8, 9);//define stepper 2 at given pins (6, 7, 8, 9)
+
+Servo ser1;//define name for servo motor
 
 int poser = 0; // initial position of server
 
 int val; // initial value of input
 
 void setup() {
-myStepper.setSpeed(40);
-myStepper2.setSpeed(40);
-myStepper3.setSpeed(20);
+
+myStepper1.setSpeed(40);//Set speed of stepper
+
+myStepper2.setSpeed(40);//Set speed of stepper
+
 Serial.begin(9600); // Serial comm begin at 9600bps
-ser1.attach(A1);
+
+ser1.attach(A1); //define pin for servo motor
 
 }
 
@@ -31,7 +40,7 @@ if (val == 'd') //if value input is equals to d
 
 {
 
-poser += 1; //than position of servo motor increases by 1 ( anti clockwise)
+poser += 1; //then position of servo motor increases by 1 ( anti clockwise)
 
 ser1.write(poser);// the servo will move according to position
 
@@ -43,7 +52,7 @@ if (val == 'a') //if value input is equals to a
 
 {
 
-poser -= 1; //than position of servo motor decreases by 1 (clockwise)
+poser -= 1; //then position of servo motor decreases by 1 (clockwise)
 
 ser1.write(poser);// the servo will move according to position
 
@@ -51,70 +60,52 @@ delay(15);//delay for the servo to get to the position
 
 }
 
-if (val == 'o') //if value input is equals to a
+if (val == 'o') //if value input is equals to o
 
 {
 
-Serial.println("clockwise");
-myStepper3.step(stepsPerRevolution);
+Serial.println("clockwise");//clockwise motion
 
-delay(15);//delay for the servo to get to the position
+myStepper1.step(stepsPerRevolution);//confirm clockwise motion
+
+delay(15);//delay for the stepper to get to the position
 
 }
 {
 
-if (val == 'p') //if value input is equals to d
+if (val == 'p') //if value input is equals to p
 
 {
 
-Serial.println("counterclockwise");
-myStepper3.step(-stepsPerRevolution);
-delay(15);//delay for the servo to get to the position
+Serial.println("counterclockwise");//counterclockwise motion
+
+myStepper1.step(-stepsPerRevolution);//counterclockwise steps
+
+delay(15);//delay for the stepper to get to the position
 
 }
-if (val == 'k') //if value input is equals to a
+if (val == 'k') //if value input is equals to k
 
 {
 
-Serial.println("clockwise");
-myStepper2.step(stepsPerRevolution);
+Serial.println("clockwise");//clockwise motion
 
-delay(15);//delay for the servo to get to the position
+myStepper2.step(stepsPerRevolution);//clockwise steps
 
-}
-{
-
-if (val == 'l') //if value input is equals to d
-
-{
-
-Serial.println("counterclockwise");
-myStepper2.step(-stepsPerRevolution);
-delay(15);//delay for the servo to get to the position
-
-}
-
-if (val == 'n') //if value input is equals to a
-
-{
-
-Serial.println("clockwise");
-myStepper.step(stepsPerRevolution);
-
-delay(15);//delay for the servo to get to the position
+delay(15);//delay for the stepper to get to the position
 
 }
 {
 
-if (val == 'm') //if value input is equals to d
+if (val == 'l') //if value input is equals to l
 
 {
 
-Serial.println("counterclockwise");
-myStepper.step(-stepsPerRevolution);
-delay(15);//delay for the servo to get to the position
+Serial.println("counterclockwise");//counterclockwise motion
+
+myStepper2.step(-stepsPerRevolution);//counterclockwise steps
+
+delay(15);//delay for the stepper to get to the position
 
 }
-
-
-}}}}}
+}}}}
